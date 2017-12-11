d53be5c457f7d98f9261282b2745a2283249ab51
YoshinoriN <nayo-msoft@outlook.jp>
2017-12-11 22:44:25 +0900

    Add menu on sidebar

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -1,10 +1,18 @@
 import io.github.gitbucket.solidbase.model.Version
+import gitbucket.core.controller.Context
+import gitbucket.core.plugin._
+import gitbucket.core.service.RepositoryService.RepositoryInfo
+import gitbucket.codesearch.controllers.CodeSearchController
 
 class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId: String = "codesearch"
   override val pluginName: String = "Code Search Plugin"
-  override val description: String = "TODO"
+  override val description: String = "Instead of removed default code search."
   override val versions: List[Version] = List(new Version("1.0.0"))
+
+  override val repositoryMenus = Seq(
+    (repository: RepositoryInfo, context: Context) => Some(Link("Code search", "Code search", "/search", Some("search")))
+  )
 
   override val controllers = Seq(
     "/*" -> new CodeSearchController
