791405fb9b8c735fa085d610411c730e1c23e947
YoshinoriN <nayo-msoft@outlook.jp>
2017-12-11 22:44:05 +0900

    Rename

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -1,5 +1,4 @@
 import io.github.gitbucket.solidbase.model.Version
-import gitbucket.codesearch.controllers.SearchController
 
 class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId: String = "codesearch"
@@ -8,6 +7,6 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val versions: List[Version] = List(new Version("1.0.0"))
 
   override val controllers = Seq(
-    "/*" -> new SearchController()
+    "/*" -> new CodeSearchController
   )
 }
--- src/main/scala/gitbucket/codeSearch/controllers/CodeSearchController.scala
+++ src/main/scala/gitbucket/codeSearch/controllers/CodeSearchController.scala
@@ -2,6 +2,6 @@ package gitbucket.codesearch.controllers
 
 import gitbucket.core.controller.ControllerBase
 
-class SearchController extends ControllerBase {
+class CodeSearchController extends ControllerBase {
 
 }
